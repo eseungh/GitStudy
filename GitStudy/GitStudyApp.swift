e2bc79823c134e6baff78e47820f29afb776649d
eseungh <eseungh@gmail.com>
2023-03-06 21:27:21 +0900

    Initial Commit

---
+++ GitStudy/GitStudyApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitStudyApp.swift
+//  GitStudy
+//
+//  Created by Seungho on 2023/03/06.
+//
+
+import SwiftUI
+
+@main
+struct GitStudyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
